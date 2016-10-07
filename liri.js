cbfb0ab133ba6bffd0f75be77992ab92a26ff261
Brandon <blshort43@gmail.com>
2016-10-07 18:59:49 -0500

    finished spotify npm

--- liri.js
+++ liri.js
@@ -4,11 +4,13 @@ var request = require('request');
 var Twitter = require('twitter');
 var spotify = require('spotify');
 
+
 var client = new Twitter(keysVar);
 
 if (process.argv[2] === "my-tweets") {
+
     client.get('statuses/user_timeline', { screen_name: "JimGaffigan", count: 5 }, function(error, tweets, response) {
-        // console.log(tweets);
+
         for (var i = 0; i < 5; i++) {
             console.log(tweets[i].text + '\n' + "Created on: " + tweets[i].created_at + '\n');
         }
@@ -16,21 +18,43 @@ if (process.argv[2] === "my-tweets") {
 };
 
 
-query = process.argv[2]
+if (query = process.argv.slice(2).join(" ")) {
+	console.log(query);
+    spotify.search({ type: 'track', query }, function(err, data) {
+    	//log any errors
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        //run search results
+        } else if (!err) {
+            console.log("\n");
+            console.log("--------------------Search Results--------------------");
+            console.log("Artists: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Preview: " + data.tracks.items[0].preview_url);
+            console.log("Album name: " + data.tracks.items[0].album.name + "\n");
+            console.log("--------------------Search Results--------------------");
+            console.log("\n");
+        }
+    });
 
-spotify.search({ type: 'track', query}, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
+} else if (query = " ") {
 
-for (var i=0; i<1; i++){
-	for(var j=0; j<2; j++){
-	console.log("\n");
-    console.log("Artists: " + data.tracks.items[i].artists[j].name);
-    console.log("Song: " + data.tracks.items[i].name);
-    console.log("Preview: " + data.tracks.items[i].preview_url);
-    console.log("Album name: " + data.tracks.items[i].album.name + "\n");
-}
-}
-});
+    spotify.lookup({ type: 'album', id: '37UgOnkBN4ZfY1nBoSCL9L' }, function(err, data) {
+    	//log any errors
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        //run search results
+        } else if (!err) {
+            console.log("\n");
+            console.log("--------------------Search Results--------------------");
+            console.log("Artists: " + data.artists[0].name);
+            console.log("Song: " + data.tracks.items[3].name);
+            console.log("Preview: " + data.tracks.items[3].preview_url);
+            console.log("Album name: " + data.name);
+            console.log("------------------------------------------------------");
+            console.log("\n");
+        }
+    });
+};
