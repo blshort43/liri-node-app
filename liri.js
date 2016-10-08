aebafebd1293602a74dbd7a792354fe0ee8dfa80
Brandon <blshort43@gmail.com>
2016-10-07 21:11:46 -0500

    cleaned up code

--- liri.js
+++ liri.js
@@ -24,45 +24,45 @@ function tweetsList() {
 
 // Spotify code:
 function spotifySearch() {
-	if (process.argv[2] && process.argv[3]) {
-	spotify.search({ type: 'track', query }, function(err, data) {
-        //log any errors
-        if (err) {
-            console.log('Error occurred: ' + err);
-            return;
-            //run search results
-        } else if (!err) {
-            console.log("\n");
-            console.log("--------------------Search Results--------------------");
-            console.log("Artists: " + data.tracks.items[0].artists[0].name);
-            console.log("Song: " + data.tracks.items[0].name);
-            console.log("Preview: " + data.tracks.items[0].preview_url);
-            console.log("Album name: " + data.tracks.items[0].album.name + "\n");
-            console.log("--------------------Search Results--------------------");
-            console.log("\n");
-        }
-    });
+    if (process.argv[2] && process.argv[3]) {
+        spotify.search({ type: 'track', query }, function(err, data) {
+            //log any errors
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+                //run search results
+            } else if (!err) {
+                console.log("\n");
+                console.log("--------------------Search Results--------------------");
+                console.log("Artists: " + data.tracks.items[0].artists[0].name);
+                console.log("Song: " + data.tracks.items[0].name);
+                console.log("Preview: " + data.tracks.items[0].preview_url);
+                console.log("Album name: " + data.tracks.items[0].album.name + "\n");
+                console.log("--------------------Search Results--------------------");
+                console.log("\n");
+            }
+        });
 
-} else if (process.argv[2]) {
+    } else if (process.argv[2]) {
 
-    spotify.lookup({ type: 'album', id: '37UgOnkBN4ZfY1nBoSCL9L' }, function(err, data) {
-        //log any errors
-        if (err) {
-            console.log('Error occurred: ' + err);
-            return;
-            //run search results
-        } else if (!err) {
-            console.log("\n");
-            console.log("--------------------Search Results--------------------");
-            console.log("Artists: " + data.artists[0].name);
-            console.log("Song: " + data.tracks.items[3].name);
-            console.log("Preview: " + data.tracks.items[3].preview_url);
-            console.log("Album name: " + data.name);
-            console.log("------------------------------------------------------");
-            console.log("\n");
-        }
-    });
-};
+        spotify.lookup({ type: 'album', id: '37UgOnkBN4ZfY1nBoSCL9L' }, function(err, data) {
+            //log any errors
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+                //run search results
+            } else if (!err) {
+                console.log("\n");
+                console.log("--------------------Search Results--------------------");
+                console.log("Artists: " + data.artists[0].name);
+                console.log("Song: " + data.tracks.items[3].name);
+                console.log("Preview: " + data.tracks.items[3].preview_url);
+                console.log("Album name: " + data.name);
+                console.log("------------------------------------------------------");
+                console.log("\n");
+            }
+        });
+    };
 }
 
 //OMDB code:
@@ -98,8 +98,8 @@ function movieSearch() {
 
 if (process.argv[2] === "my-tweets") {
     tweetsList();
-}else if (process.argv[2] === "spotify-this-song") {
+} else if (process.argv[2] === "spotify-this-song") {
     spotifySearch();
-}else if (process.argv[2] === "movie-this") {
+} else if (process.argv[2] === "movie-this") {
     movieSearch();
 };
