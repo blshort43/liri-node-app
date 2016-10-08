0d98284b43231d517af292ae714fb7b064455040
Brandon <blshort43@gmail.com>
2016-10-08 13:51:27 -0500

    cleaned up spotify code

--- liri.js
+++ liri.js
@@ -25,41 +25,24 @@ function tweetsList() {
 
 // Spotify code:
 function spotifySearch() {
-    if (process.argv[2] && process.argv[3]) {
-        spotify.search({ type: 'track', query }, function(err, data) {
-            //log any errors
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
-                //run search results
-            } else if (!err) {
-                console.log("\n--------------------Search Results--------------------");
-                console.log("Artists: " + data.tracks.items[0].artists[0].name);
-                console.log("Song: " + data.tracks.items[0].name);
-                console.log("Preview: " + data.tracks.items[0].preview_url);
-                console.log("Album name: " + data.tracks.items[0].album.name + "\n");
-                console.log("--------------------Search Results--------------------\n");
-            }
-        });
-
-    } else if (process.argv[2]) {
-
-        spotify.lookup({ type: 'album', id: '37UgOnkBN4ZfY1nBoSCL9L' }, function(err, data) {
-            //log any errors
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
-                //run search results
-            } else if (!err) {
-                console.log("\n--------------------Search Results--------------------");
-                console.log("Artists: " + data.artists[0].name);
-                console.log("Song: " + data.tracks.items[3].name);
-                console.log("Preview: " + data.tracks.items[3].preview_url);
-                console.log("Album name: " + data.name);
-                console.log("------------------------------------------------------\n");
-            }
-        });
+    if (query == '') {
+        query = "Ace of Base - The Sign";
     };
+    spotify.search({ type: 'track', query }, function(err, data) {
+        //log any errors
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+            //run search results
+        } else if (!err) {
+            console.log("\n--------------------Search Results--------------------");
+            console.log("Artists: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Preview: " + data.tracks.items[0].preview_url);
+            console.log("Album name: " + data.tracks.items[0].album.name + "\n");
+            console.log("--------------------Search Results--------------------\n");
+        }
+    });
 }
 
 //OMDB code:
